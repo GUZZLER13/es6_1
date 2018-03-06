04b9deed0e6351b997f086214e777f3b03aab535
Stéphane Da Silveira <stephanedasil@gmail.com>
2018-03-06 13:47:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let city = prompt("Où habitez-vous ?");
+let age = prompt("Quel âge avez-vous ? (juste le nombre svp)");
+
+const dateOfBirth = prompt("Quel est votre date de naissance ?");
+
+let sentence = `J'habite à ${city}, j'ai ${age} ans, et je suis né le ${dateOfBirth}, mais ça je le savais déjà non ?!`;
+
+alert(sentence);
+
+// merci pour la correction !
